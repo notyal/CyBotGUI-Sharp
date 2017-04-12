fa6ec1b03da89503e524042b5f7c23f4528f7046
Kyle <Kyle@KYLE>
2017-04-12 14:32:23 -0500

    Add project files.

---
+++ Cybot GUI/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Cybot_GUI
+{
+    partial class CyBotGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CyBotGUI));
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearLogsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearGraphToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitAltF4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.forwardValue = new System.Windows.Forms.TextBox();
+            this.leftValue = new System.Windows.Forms.TextBox();
+            this.rightValue = new System.Windows.Forms.TextBox();
+            this.obstacleGraph = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacleGraph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(12, 27);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(209, 20);
+            this.maskedTextBox1.TabIndex = 0;
+            this.maskedTextBox1.Text = "127.0.0.1";
+            this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 157);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(289, 290);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(929, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearLogsToolStripMenuItem,
+            this.clearGraphToolStripMenuItem,
+            this.exitAltF4ToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // clearLogsToolStripMenuItem
+            // 
+            this.clearLogsToolStripMenuItem.Name = "clearLogsToolStripMenuItem";
+            this.clearLogsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.clearLogsToolStripMenuItem.Text = "Clear Logs";
+            this.clearLogsToolStripMenuItem.Click += new System.EventHandler(this.clearLogsToolStripMenuItem_Click);
+            // 
+            // clearGraphToolStripMenuItem
+            // 
+            this.clearGraphToolStripMenuItem.Name = "clearGraphToolStripMenuItem";
+            this.clearGraphToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.clearGraphToolStripMenuItem.Text = "Clear Graph";
+            this.clearGraphToolStripMenuItem.Click += new System.EventHandler(this.clearGraphToolStripMenuItem_Click);
+            // 
+            // exitAltF4ToolStripMenuItem
+            // 
+            this.exitAltF4ToolStripMenuItem.Name = "exitAltF4ToolStripMenuItem";
+            this.exitAltF4ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitAltF4ToolStripMenuItem.Text = "Exit (Alt+F4)";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Forward (mm)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 120);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Right (Degrees)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 91);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Left (Degrees)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // forwardValue
+            // 
+            this.forwardValue.Location = new System.Drawing.Point(114, 62);
+            this.forwardValue.MaxLength = 4;
+            this.forwardValue.Name = "forwardValue";
+            this.forwardValue.Size = new System.Drawing.Size(44, 20);
+            this.forwardValue.TabIndex = 7;
+            this.forwardValue.Text = "100";
+            this.forwardValue.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.forwardValue.TextChanged += new System.EventHandler(this.forwardValue_TextChanged);
+            // 
+            // leftValue
+            // 
+            this.leftValue.Location = new System.Drawing.Point(114, 91);
+            this.leftValue.Name = "leftValue";
+            this.leftValue.Size = new System.Drawing.Size(44, 20);
+            this.leftValue.TabIndex = 8;
+            this.leftValue.Text = "90";
+            this.leftValue.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.leftValue.TextChanged += new System.EventHandler(this.leftValue_TextChanged);
+            // 
+            // rightValue
+            // 
+            this.rightValue.Location = new System.Drawing.Point(114, 123);
+            this.rightValue.Name = "rightValue";
+            this.rightValue.Size = new System.Drawing.Size(44, 20);
+            this.rightValue.TabIndex = 9;
+            this.rightValue.Text = "90";
+            this.rightValue.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.rightValue.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // obstacleGraph
+            // 
+            this.obstacleGraph.Location = new System.Drawing.Point(309, 27);
+            this.obstacleGraph.Name = "obstacleGraph";
+            this.obstacleGraph.Size = new System.Drawing.Size(608, 420);
+            this.obstacleGraph.TabIndex = 10;
+            this.obstacleGraph.TabStop = false;
+            // 
+            // CyBotGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 459);
+            this.Controls.Add(this.obstacleGraph);
+            this.Controls.Add(this.rightValue);
+            this.Controls.Add(this.leftValue);
+            this.Controls.Add(this.forwardValue);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "CyBotGUI";
+            this.Text = "CyBot GUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacleGraph)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearLogsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearGraphToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitAltF4ToolStripMenuItem;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox forwardValue;
+        private System.Windows.Forms.TextBox leftValue;
+        private System.Windows.Forms.TextBox rightValue;
+        private System.Windows.Forms.PictureBox obstacleGraph;
+    }
+}
+
