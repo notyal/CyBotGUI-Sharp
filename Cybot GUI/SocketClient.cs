0a9564af79ebc3ad6df7ff18f02c25d3948507e8
Layton Nelson <notyal.dev@gmail.com>
2017-04-13 18:25:35 -0500

    Implement receive functionality

---
+++ Cybot GUI/SocketClient.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Cybot_GUI
+{
+	public class SocketClient
+	{
+
+		/// <summary>
+		/// Get or set the port to connect to.
+		/// </summary>
+		public int Port;
+
+		static Exception lastException;
+		IPAddress ip;
+		Socket socket = null;
+
+		/// <summary>
+		/// Get or set the Host IP address as a string.
+		/// </summary>
+		/// <value>The IP Address.</value>
+		public string Ip {
+			get { return ip.ToString(); }
+
+			set {
+				if (!IPAddress.TryParse(value, out ip))
+					throw new Exception("Error: IP Address format could not be parsed.");
+			}
+		}
+
+		/// <summary>
+		/// Gets the last error message.
+		/// Only updated when an exception is thrown.
+		/// Not updated upon success.
+		/// </summary>
+		/// <value>The error message if it exists, otherwise an empty string.</value>
+		public string LastErrorMessage {
+			get {
+				if (lastException == null) return "";
+				return lastException.Message;
+			}
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:CyBotGtk.SocketClient"/> class
+		/// with a default port of 42880.
+		/// </summary>
+		/// <param name="Host">Host IP Address.</param>
+		/// <param name="Port">Port if not 42880.</param>
+		public SocketClient(string Host, int Port = 42880)
+		{
+			Ip = Host;
+			this.Port = Port;
+		}
+
+		/// <summary>
+		/// Attempt to connect to the server.
+		/// </summary>
+		/// <returns><c>true</c>, if connected successfully, <c>false</c> otherwise.</returns>
+		public bool Connect()
+		{
+			try {
+				socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+				socket.Connect(ip, Port);
+			} catch (SocketException ex) {
+				lastException = ex;
+			} catch (Exception ex) {
+				WriteException(ex);
+			}
+
+			if (socket == null) return false;
+			return socket.Connected;
+		}
+
+		/// <summary>
+		/// Disconnect from the server.
+		/// </summary>
+		/// <returns><c>true</c>, if not connected to server, <c>false</c> otherwise.</returns>
+		public bool Disconnect()
+		{
+			if (socket == null) return true;
+
+			// close socket without needing to reuse it
+			try {
+				socket.Disconnect(false);
+			} catch (Exception ex) {
+				WriteException(ex);
+				return false;
+			}
+
+			return !socket.Connected;
+		}
+
+		/// <summary>
+		/// If connected to server.
+		/// </summary>
+		/// <returns><c>true</c>, if connected to server, <c>false</c> otherwise.</returns>
+		public bool IsConnected()
+		{
+			if (socket == null) return false;
+			return socket.Connected;
+		}
+
+		/// <summary>
+		/// Write the specified message to the server.
+		/// </summary>
+		/// <returns>If write was successful.</returns>
+		/// <param name="msg">Message to write.</param>
+		public bool Write(string msg)
+		{
+			if (socket == null || !socket.Connected) return false;
+
+			try {
+				socket.Send(Encoding.ASCII.GetBytes(msg));
+				return true;
+			} catch (Exception ex) {
+				WriteException(ex);
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// Thread that processes received data
+		/// </summary>
+		/// <param name="output">Output.</param>
+		public void ReceiveThread(IProgress<string> output, CancellationToken ct)
+		{
+			if (socket == null || !socket.Connected) return;
+
+			while (!ct.IsCancellationRequested) {
+				byte[] bytes = new byte[256];
+				int size;
+				try {
+					// receive data (blocking)
+					size = socket.Receive(bytes);
+
+					// only report data if buffer is >0
+					if (size > 0) {
+						String inputSerial = Encoding.UTF8.GetString(bytes);
+						output.Report(inputSerial);
+					}
+				} catch (Exception ex) {
+					WriteException(ex);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Utility to write an exception to the console.
+		/// </summary>
+		/// <param name="ex">Exception.</param>
+		public static void WriteException(Exception ex)
+		{
+			lastException = ex;
+			Console.WriteLine();
+			Console.WriteLine("Caught Exception : " + ex.Message);
+			Console.WriteLine(ex);
+			Console.WriteLine();
+		}
+	}
+}
--- Cybot GUI/Window.cs
+++ Cybot GUI/Window.cs
@@ -5,106 +5,200 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Cybot_GUI
 {
-    public partial class CyBotGUI : Form
-    {
-        public CyBotGUI()
-        {
-            InitializeComponent();
-        }
+	public partial class CyBotGUI : Form
+	{
 
-        //Connect
-        private void ConnectButton_Click(object sender, EventArgs e)
-        {
-            logBox.Items.Add("Attempting connection to " + maskedTextBox1.Text);
-        }
+		readonly string connectText = "Connect";
+		readonly string disconnectText = "Disconnect";
+		SocketClient client;
+		static CancellationTokenSource ReceiveThreadCancel = new CancellationTokenSource();
 
-        private void logBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
+		public CyBotGUI()
+		{
+			InitializeComponent();
+			ConnectButton.Text = connectText;
+			maskedTextBox1.Text = "127.0.0.1";
 
-        }
+		}
 
-        //
-        //Text boxes for the movement controls.
-        //
-        private void rightValue_TextChanged(object sender, EventArgs e)
-        {
+		protected override void OnFormClosing(FormClosingEventArgs e)
+		{
+			base.OnFormClosing(e);
+			if (PreClosingConfirmation() == DialogResult.Yes) {
+				Dispose(true);
 
-        }
+				// close receiveThread if it is running
+				ReceiveThreadCancel.Cancel();
 
-        private void leftValue_TextChanged(object sender, EventArgs e)
-        {
+				Application.Exit();
+			} else {
+				e.Cancel = true;
+			}
+		}
 
-        }
+		private DialogResult PreClosingConfirmation()
+		{
+			DialogResult res = MessageBox.Show(
+				"Do you want to quit?", // question
+				"Quit...", // title
+				MessageBoxButtons.YesNo,
+				MessageBoxIcon.Question
+			);
+			return res;
+		}
 
-        private void forwardValue_TextChanged(object sender, EventArgs e)
-        {
+		//Connect
+		private async void ConnectButton_Click(object sender, EventArgs e)
+		{
+			if (ConnectButton.Text == connectText) {
+				// connected
+				ConnectButton.Text = disconnectText;
 
-        }
+				// setup socketclient
+				client = new SocketClient(maskedTextBox1.Text);
+				WriteToLog(string.Format("Trying to connect to {0}:{1}...", client.Ip, client.Port));
 
-        //Forward Button
-        private void forwardButton_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                int fV = Convert.ToInt16(forwardValue.Text);
-                if (fV > 0)
-                {
-                    logBox.Items.Add("Moving forward " + fV + "mm");
-                }
-            }
-            catch (Exception a)
-            {
-                logBox.Items.Add("INVALID FORMAT");
-                logBox.Items.Add(a.Message);
-                throw;
-            }
+				// check if connected
+				if (!client.Connect()) {
+					WriteToLog(client.LastErrorMessage + "...\n");
+					ConnectButton.Text = connectText;
+				} else {
+					// actions to perform after successful connection
+					// http://stackoverflow.com/a/18033198
+					var output = new Progress<string>(s => WriteToLog(s, false));
+					await Task.Factory.StartNew(() => client.ReceiveThread(output, ReceiveThreadCancel.Token), TaskCreationOptions.LongRunning);
+					WriteToLog("Ended receive thread.\n");
+				}
 
-        }
-        private void addObject(int x, int y)
-        {
-            System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
-            System.Drawing.Graphics formGraphics;
-            formGraphics = this.CreateGraphics();
-            formGraphics.FillRectangle(myBrush, new Rectangle(0, 0, 200, 300));
-            myBrush.Dispose();
-            formGraphics.Dispose();
-        }
+			} else {
+				// disconnected
+				ConnectButton.Text = connectText;
 
-        //Left Button
-        private void leftButton_Click(object sender, EventArgs e)
-        {
-            addObject(0, 0);
-        }
+				WriteToLog("Disconnecting...");
 
-        //Right Button
-        private void rightButton_Click(object sender, EventArgs e)
-        {
+				// tell receive to cancel
+				ReceiveThreadCancel.Cancel();
 
-        }
+				if (!client.Disconnect()) {
+					WriteToLog(client.LastErrorMessage + "...\n");
+					ConnectButton.Text = connectText;
+				} else {
+					WriteToLog("Disconnection done.\n");
+				}
+			}
+		}
 
-        private void clearGraphToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            
-        }
+		private void logBox_SelectedIndexChanged(object sender, EventArgs e)
+		{
 
-        private void clearLogsToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            logBox.Items.Clear();
-        }
+		}
 
-        private void Window_Load(object sender, EventArgs e)
-        {
+		//
+		//Text boxes for the movement controls.
+		//
+		private void rightValue_TextChanged(object sender, EventArgs e)
+		{
 
-        }
+		}
 
-        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
-        {
+		private void leftValue_TextChanged(object sender, EventArgs e)
+		{
 
-        }
-    }
+		}
+
+		private void forwardValue_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		//Forward Button
+		private void forwardButton_Click(object sender, EventArgs e)
+		{
+			try {
+				int fV = Convert.ToInt16(forwardValue.Text);
+				if (fV > 0) {
+					logBox.Items.Add("Moving forward " + fV + "mm");
+				}
+			} catch (Exception a) {
+				logBox.Items.Add("INVALID FORMAT");
+				logBox.Items.Add(a.Message);
+				throw;
+			}
+
+		}
+		private void addObject(int x, int y)
+		{
+			System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+			System.Drawing.Graphics formGraphics;
+			formGraphics = this.CreateGraphics();
+			formGraphics.FillRectangle(myBrush, new Rectangle(0, 0, 200, 300));
+			myBrush.Dispose();
+			formGraphics.Dispose();
+		}
+
+		//Left Button
+		private void leftButton_Click(object sender, EventArgs e)
+		{
+			addObject(0, 0);
+		}
+
+		//Right Button
+		private void rightButton_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void clearGraphToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void clearLogsToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			logBox.Items.Clear();
+		}
+
+		private void Window_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+		{
+
+		}
+
+		/// <summary>
+		/// Appends item to logBox as well as the System Console.
+		/// </summary>
+		/// <param name="msg">Message to write</param>
+		/// <param name="writeConsole">If set to <c>false</c>, disable writing to the console.</param>
+		private void WriteToLog(string msg, bool writeConsole = true)
+		{
+			logBox.Items.Add(msg);
+			if (writeConsole) Console.Write(msg);
+
+			AutoScroll(logBox);
+		}
+
+
+
+		/// <summary>
+		/// Autos the scroll.
+		/// </summary>
+		/// <param name="l">ListBox to autoscroll</param>
+		public static new void AutoScroll(ListBox l)
+		{
+			// https://stackoverflow.com/q/28285130
+			// autoscroll to end
+			l.SelectedIndex = l.Items.Count - 1;
+			l.SelectedIndex = - 1;  // deselect the item
+		}
+	}
 }
